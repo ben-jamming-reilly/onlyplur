71e30fe14ba81f041e58d39bd5bcd2a5f884e68b
Ben Reilly <benreilly0809@gmail.com>
2024-04-27 02:20:08 -0400

    all done, for rn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between ">
-      <div className="container text-white text-6xl sm:text-8xl p-4">
+    <main className="flex min-h-screen w-full flex-col items-center  justify-between ">
+      <div className="container text-white flex flex-col gap-4 text-6xl sm:text-8xl p-4 py-16">
         <h1>You are loved.</h1>
         <h1>You matter.</h1>
-        <p>The world would suck without you in it.</p>
+        <p className="text-4xl sm:text-6xl">
+          The world would suck without you in it.
+        </p>
       </div>
     </main>
   );
