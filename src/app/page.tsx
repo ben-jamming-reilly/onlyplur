a927f3ffb13418be84f7ba18b9880491a7d84b4c
Ben Reilly <benreilly0809@gmail.com>
2024-04-27 16:00:14 -0400

    mo heart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center  justify-between ">
       <div className="container text-white flex flex-col gap-4 text-6xl sm:text-8xl p-4 py-16">
-        <h1 className="text-[#ff0069] text-8xl">&lt;3</h1>
         <p>You are loved.</p>
         <p>You matter.</p>
         <p className="text-4xl sm:text-6xl">
           The world would suck without you in it.{" "}
         </p>
+        <h1 className="text-[#ff0069] text-8xl">&lt;3</h1>
       </div>
     </main>
   );
