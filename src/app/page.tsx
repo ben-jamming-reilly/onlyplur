baec2fb2c15b7a3f0bfdd706527b06b8d6b18ba9
Ben Reilly <benreilly0809@gmail.com>
2024-04-27 16:03:08 -0400

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center  justify-between ">
+    <main className="flex min-h-[100dvh] w-full flex-col items-center  justify-between ">
       <div className="container text-white flex flex-col gap-4 text-6xl sm:text-8xl p-4 py-16">
         <p>You are loved.</p>
         <p>You matter.</p>
         <p className="text-4xl sm:text-6xl">
           The world would suck without you in it.{" "}
         </p>
-        <h1 className="text-[#ff0069] text-8xl">&lt;3</h1>
+        <h1 className="text-[#ff0069] text-9xl">&lt;3</h1>
       </div>
     </main>
   );
