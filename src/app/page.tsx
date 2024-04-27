66f5991e91adc925f33212584c09bd366d69e6b3
Ben Reilly <benreilly0809@gmail.com>
2024-04-27 01:15:20 -0400

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>You are loved and matter</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white text-4xl">
+      <div>
+        <h1>You are loved.</h1>
+        <h1>You matter.</h1>
+        <p>The world would suck without you in it</p>
+      </div>
     </main>
   );
 }
