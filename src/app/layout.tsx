132bc71a52c16bccb90e836a4d90c6de3df3f445
Ben Reilly <benreilly0809@gmail.com>
2024-04-27 01:25:16 -0400

    mo fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter, Micro_5 } from "next/font/google";
+import { Micro_5, Jersey_10 } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const micro = Micro_5({ subsets: ["latin"], weight: "400" });
+const micro = Jersey_10({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "only plur",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white text-4xl">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white text-8xl">
       <div>
         <h1>You are loved.</h1>
         <h1>You matter.</h1>
